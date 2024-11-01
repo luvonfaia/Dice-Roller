7f898763445524755f534e5d63cad202cdb3efab
Alex Deaconu <luvonfaia@gmail.com>
2024-11-01 13:15:08 +0200

    Phase1
    Html & Css are 90% complete. No functions on it at this moment

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+
+function rollDice(){
+    const numOfDice = document.getElementById("numOfDice").value;
+    const diceResult = document.getElementById("diceResult").value;
+    const diceImages = document.getElementById("diceImages").value;
+}
